1dcb03d35b7cf6d8b36f276a6ba4d058a61b0b5b
Nikolay Li <nikolayli@Endeavor.local>
2026-08-26 03:38:48 -0700

    fix import

--- src/lib/reveal.ts
+++ src/lib/reveal.ts
@@ -1,5 +1,5 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import type { Action } from "svelte/action";
 
 gsap.registerPlugin(ScrollTrigger);
